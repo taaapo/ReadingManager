adb09b7b45c7b47abb9cbb301298b4ebf4ecdcf5
taaapo <takupow@icloud.com>
2023-10-16 01:13:06 +0900

    2023.10.16.1:12 RealmSwiftをPodでインストールして、AppDelegateを修正し、DataModel(Books, Content)を追加

--- ReadingManager/AppDelegate.swift
+++ ReadingManager/AppDelegate.swift
@@ -7,14 +7,21 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        do {
+            let realm = try Realm()
+        } catch {
+            print("Error initialising new realm, \(error)")
+        }
+        
         return true
     }
 
